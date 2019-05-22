6bf126e0da97e366ce19b580cea5c975107e005d
Guido Montorfano <guidomontorfano78@gmail.com>
2019-05-22 15:22:11 -0300

    errores correccion

--- js/guido.js
+++ js/guido.js
@@ -2,3 +2,16 @@ function scrollG(id){
     $("html,body").animate({scrollTop: $('#'+id).offset().top}, 1000);
 }
 
+$(document).ready(function(){
+    $("button").click(function(){
+        $("ghl").animate({
+            height: '150px', 
+            left: '500px',
+            opacity: '0.7',
+            top: '100px',
+            width: '150px'
+        }, 1500);
+    });
+});
+
+
--- js/main.js
+++ js/main.js
@@ -1,27 +1,21 @@
-/* ===================================================================
- * Glint - Main JS
- *
- * ------------------------------------------------------------------- */
+
 
 (function($) {
 
     "use strict";
     
     var cfg = {
-        scrollDuration : 800, // smoothscroll duration
+        scrollDuration : 800, 
         mailChimpURL   : 'https://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e6957d85dc'   // mailchimp url
     },
 
     $WIN = $(window);
 
-    // Add the User Agent to the <html>
-    // will be used for IE10 detection (Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0))
     var doc = document.documentElement;
     doc.setAttribute('data-useragent', navigator.userAgent);
 
 
-   /* Preloader
-    * -------------------------------------------------- */
+
     var clPreloader = function() {
         
         $("html").addClass('cl-preload');
@@ -45,8 +39,6 @@
     };
 
 
-   /* Menu on Scrolldown
-    * ------------------------------------------------------ */
     var clMenuOnScrolldown = function() {
         
         var menuTrigger = $('.header-menu-toggle');
