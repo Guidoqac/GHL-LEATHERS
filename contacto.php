1dd64477b55bdfcc7b0a7146478e7b4e92f3f1be
Guido Montorfano <guidomontorfano78@gmail.com>
2019-07-14 00:33:47 -0300

    Gallery refactor

--- contacto.php
+++ contacto.php
@@ -13,6 +13,7 @@ if ( !isset($_POST["nombre"]) || !isset($_POST["email"]) || !isset($_POST["mensa
 $nombre = $_POST["nombre"];
 $email = $_POST["email"];
 $mensaje = $_POST["mensaje"];
+$telefono = $_POST["telefono"];
 
 // Datos de la cuenta de correo utilizada para enviar vía SMTP
 $smtpHost = "c1590313.ferozo.com";  // Dominio alternativo brindado en el email de alta 
@@ -40,15 +41,24 @@ $mail->From = $email; // Email desde donde envío el correo.
 $mail->FromName = $nombre;
 $mail->AddAddress($emailDestino); // Esta es la dirección a donde enviamos los datos del formulario
 
-$mail->Subject = "DonWeb - Ejemplo de formulario de contacto"; // Este es el titulo del email.
+$mail->Subject = "Nuevo Email"; // Este es el titulo del email.
 $mensajeHtml = nl2br($mensaje);
-$mail->Body = "{$mensajeHtml} <br /><br />Formulario de ejemplo. By DonWeb<br />"; // Texto del email en formato HTML
-$mail->AltBody = "{$mensaje} \n\n Formulario de ejemplo By DonWeb"; // Texto sin formato HTML
+$mail->Body = "Nombre: {$nombre}<br/><br/>
+               Consulta: {$mensajeHtml} <br /><br />
+               Teléfono: {$telefono}"; // Texto del email en formato HTML
+$mail->AltBody = "{$mensaje}"; // Texto sin formato HTML
 // FIN - VALORES A MODIFICAR //
 
 $estadoEnvio = $mail->Send(); 
 if($estadoEnvio){
-    echo "El correo fue enviado correctamente.";
+    echo '<script type="text/javascript">
+           window.location.href="index.html#contacto";
+		   alert("Mensaje Enviado Correctamente");	           
+		   </script>';
 } else {
-    echo "Ocurrió un error inesperado.";
+    echo '<script type="text/javascript">
+		   alert("Ocurrio un error durante el envio del formulario, intente nuevamente mas tarde");	
+		   window.location.href="index.html";
+		   </script>';
 }
+
